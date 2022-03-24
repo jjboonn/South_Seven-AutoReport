078762cf79d5312f8b4abfb1b855678b1720414b
jjboonn <1582880899@qq.com>
2022-03-24 19:49:25 +0800

    Update report.py

--- report.py
+++ report.py
@@ -104,12 +104,11 @@ class Report(object):
             return flag
         else:
             print("Report SUCCESSFUL!")
-       
+'''       
         # 自动出校报备
         ret = session.get("https://weixine.ustc.edu.cn/2020/apply/daliy/i")
         #print(ret.status_code)
-        #print(ret.url)
-'''        
+        #print(ret.url)      
         if (ret.status_code == 200):
             #每日报备
             print("开始例行报备.")
